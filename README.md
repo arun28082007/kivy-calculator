07ab3a4202da6e1a38cb52ff43cece40bc2762c0
ghosteon <vladik.nichik@gmail.com>
2018-07-07 17:28:11 +0300

    v2.0

--- README.md
+++ README.md
@@ -3,8 +3,3 @@
 The simple [Calculator](https://en.m.wikipedia.org/wiki/Calculator) in Python and Kivy.
 
 ![game demo](demo.gif)
-
-
-### Bugs
-- possibility to enter two or more operators
-- after the "=" operator calculates, the result is not reset and you need to reset it manually
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 
 class Calculator(BoxLayout):
-    label = '0'
+    label = ''
     
     def delete(self, instance):
         self.display.text = instance[:0]
@@ -11,12 +11,13 @@ class Calculator(BoxLayout):
         self.display.text = instance[:-1]
     
     def calc(self, instance):
-        try:
-            self.display.text = str( eval(instance) )
-        except Exception:
-            self.display.text = 'nil'
-
+        self.display.text = str(eval(instance))
+        self.result.text = str(eval(instance))
+        
 class CalculatorApp(App):
+    trigger = False
+    triggerClear = False
+    
     def build(self):
         return Calculator()
         
